6535cb23ce8120aa4dd1318a51564e5bf5fe7be2
Nicolás Vargas <elias.nicolas.vargas@gmail.com>
2025-07-09 19:07:23 -0300

    Arreglando conexion local

--- src/pages/Torneos.js
+++ src/pages/Torneos.js
@@ -7,7 +7,7 @@ export default function Torneos() {
   const [torneoSeleccionado, setTorneoSeleccionado] = useState(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/torneos/actuales")
+    fetch(`${process.env.REACT_APP_API_URL}/torneos/actuales`)
       .then((res) => res.json())
       .then((data) => setTorneos(data))
       .catch((error) => console.error("Error al cargar torneos:", error));
