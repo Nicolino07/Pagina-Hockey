11684ab2498f02b333d9295f2a8e3957c971a0eb
Nicolás Vargas <elias.nicolas.vargas@gmail.com>
2025-07-24 09:29:16 -0300

    Mejorar indexacion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/NavBar";
 import Inicio from "./pages/Inicio";
@@ -8,43 +8,36 @@ import Torneos from "./pages/Torneos";
 import Resultados from "./pages/Resultados";
 import Footer from "./components/Footer";
 
-/*Importar equipos */
 import Huemules from "./equipos/Huemules";
 import Wiliches from "./equipos/Wiliches";
 import Estudiantes from "./equipos/Estudiantes";
-/* Importar Galerias */
+
 import PrimeraCaballeros from "./galeria/PrimeraCaballeros";
 import PrimeraDamas from "./galeria/PrimeraDamas";
 import Inferiores from "./galeria/Inferiores";
 
 import "./App.css"; 
 
-
-
 function App() {
   return (
     <div className="principal">
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
-          
-          <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route path="/resultados" element={<Resultados />} />
-            <Route path="/resultados/:categoria" element={<Resultados />} />
-            <Route path="/torneos" element={<Torneos />} />
-            <Route path="/fixture" element={<Fixture />} />
-            <Route path="/equipos/Huemules" element={<Huemules />} />
-            <Route path="/equipos/Wiliches" element={<Wiliches />} />
-            <Route path="/equipos/Estudiantes" element={<Estudiantes />} />
-            <Route path="/galeria/PrimeraDamas" element={<PrimeraDamas />} />
-            <Route path="/galeria/PrimeraCaballeros" element={<PrimeraCaballeros />} />
-            <Route path="/galeria/Inferiores" element={<Inferiores />} />
-          </Routes>
-          
-          <Footer /> {/* Footer fijo en todas las páginas */}
-        
-      </BrowserRouter>
-
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/resultados" element={<Resultados />} />
+          <Route path="/resultados/:categoria" element={<Resultados />} />
+          <Route path="/torneos" element={<Torneos />} />
+          <Route path="/fixture" element={<Fixture />} />
+          <Route path="/equipos/Huemules" element={<Huemules />} />
+          <Route path="/equipos/Wiliches" element={<Wiliches />} />
+          <Route path="/equipos/Estudiantes" element={<Estudiantes />} />
+          <Route path="/galeria/PrimeraDamas" element={<PrimeraDamas />} />
+          <Route path="/galeria/PrimeraCaballeros" element={<PrimeraCaballeros />} />
+          <Route path="/galeria/Inferiores" element={<Inferiores />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
