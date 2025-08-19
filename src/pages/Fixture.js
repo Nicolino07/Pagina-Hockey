321126032f53d120c7fd28d43d11a443a185d266
Nicolás Vargas <elias.nicolas.vargas@gmail.com>
2025-08-19 14:52:59 -0300

    Nuevos horarios

--- src/pages/Fixture.js
+++ src/pages/Fixture.js
@@ -5,67 +5,73 @@ export default function Fixture() {
 
   const fixture = [
     {
-      liga: "Damas A - Fecha #1",
+      liga: "Damas A",
       fechas: [
         {
-          dia: "Sabado 09 agosto",
+          dia: "Sabado 23 agosto",
           juegos: [
-            { hora: "12:00", local: "Vuriclub", vs: "Pehunes", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "13:00", local: "Eventos", vs: "San Esteban", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "14:00", local: "Williches", vs: "Estudiantes", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "17:00", local: "Vuriclub", vs: "San Esteban", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "18:00", local: "Esc. Municipal", vs: "Estudiantes", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "19:00", local: "Eventos", vs: "Williches", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "10:00", local: "San Esteban A", vs: "Pehuenes A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "11:00", local: "Vuriclub", vs: "Williches", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "12:00", local: "Esc. Municipal", vs: "Eventos", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "16:00", local: "Vuriclub", vs: "Esc. Municipal", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "17:00", local: "San Esteban A", vs: "Williches", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "18:00", local: "Estudiantes", vs: "Pehuenes A", ubicacion: "Polideportivo Dina Huapi" },
           ],
         },
         {
-          dia: "Domingo 10 agosto",
+          dia: "Domingo 24 agosto",
           juegos: [
-            { hora: "12:00", local: "Hualas", vs: "Esc. Municipal", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "16:00", local: "Pehuenes", vs: "Hualas", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "10:30", local: "Hualas", vs: "Estudiantes", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "17:30", local: "Hualas", vs: "Eventos", ubicacion: "Polideportivo Dina Huapi" },
           ],
         },
       ],
     },
     {
-      liga: "Caballeros - Fecha #1",
+      liga: "Caballeros",
       fechas: [
         {
-          dia: "Sabado 09 agosto",
+          dia: "Sabado 23 agosto",
           juegos: [
-            { hora: "15:00", local: "Estudiantes", vs: "Williches", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "16:00", local: "Esc. Municipal A", vs: "Esc. Municipal B", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "20:00", local: "Huemules", vs: "Esc. Municipal B", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "21:00", local: "San Esteban", vs: "Esc. Municipal A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "13:00", local: "Williches", vs: "Esc. Municipal A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "14:00", local: "Estudiantes", vs: "Huemules", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "15:00", local: "San Esteban", vs: "Esc. Municipal B", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "19:00", local: "El Bolson", vs: "Esc. Municipal B", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "20:00", local: "Esc. Municipal A", vs: "Huemules", ubicacion: "Polideportivo Dina Huapi" },
           ],
         },
         {
-          dia: "Domingo 10 agosto",
+          dia: "Domingo 24 agosto",
           juegos: [
-            { hora: "13:00", local: "Huemules", vs: "Lacar", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "14:00", local: "Esc. Municipal B", vs: "Hualas", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "15:00", local: "El Bolson", vs: "San Esteban", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "17:00", local: "Estudiantes", vs: "Hualas", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "18:00", local: "El Bolson", vs: "Lacar", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "12:30", local: "San Esteban", vs: "Lacar", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "13:30", local: "El Bolson", vs: "Williches", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "14:30", local: "Huemules", vs: "Hualas", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "18:30", local: "Lacar", vs: "Esc. Municipal A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "19:30", local: "Williches", vs: "Hualas", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "20:30", local: "El Bolson", vs: "Estudiantes", ubicacion: "Polideportivo Dina Huapi" },
           ],
         },
       ],
     },
     {
-      liga: "Damas B - Fecha #1",
+      liga: "Damas B",
       fechas: [
         {
-          dia: "Sabado 09 agosto",
+          dia: "Sabado 23 agosto",
           juegos: [
-            { hora: "10:00", local: "Patagon", vs: "Huemules B", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "11:00", local: "San Esteban B", vs: "San Esteban A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "16:00", local: "El Bolson B", vs: "Cohiues", ubicacion: "New Lawn" },
+            { hora: "17:00", local: "Patagon", vs: "San Esteban C", ubicacion: "New Lawn" },
+            { hora: "18:00", local: "El Bolson B", vs: "Huemules B", ubicacion: "New Lawn" },
+            { hora: "19:00", local: "Cohiues", vs: "Pehuenes B", ubicacion: "New Lawn" },
           ],
         },
         {
-          dia: "Domingo 10 agosto",
+          dia: "Domingo 24 agosto",
           juegos: [
-            { hora: "10:00", local: "Pehuenes", vs: "Huemules A", ubicacion: "Polideportivo Dina Huapi" },
-            { hora: "11:00", local: "Cohiues", vs: "La Vieja Guardia", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "09:30", local: "Pehuenes B", vs: "San Esteban B", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "11:30", local: "Lacar", vs: "El Bolson A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "15:30", local: "Lacar", vs: "Huemules A", ubicacion: "Polideportivo Dina Huapi" },
+            { hora: "16:30", local: "El Bolson A", vs: "La Vieja Guardia", ubicacion: "Polideportivo Dina Huapi" },
           ],
         },
       ],
